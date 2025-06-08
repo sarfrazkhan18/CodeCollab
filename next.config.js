99cebb7acc9cbf6010109a4b6ca55d12870c23d8
Awan189 <maliksarfrazawan0@gmail.com>
2025-06-08 16:48:59 +0500

    Fix SWC compilation error

--- next.config.js
+++ next.config.js
@@ -7,7 +7,28 @@ const nextConfig = {
     unoptimized: true 
   },
   output: 'export',
-  reactStrictMode: false
+  reactStrictMode: false,
+  // Completely disable SWC to avoid WebContainer compatibility issues
+  swcMinify: false,
+  compiler: {
+    // Disable SWC transforms
+    removeConsole: false,
+  },
+  // Use Babel instead of SWC
+  experimental: {
+    forceSwcTransforms: false,
+  },
+  // Webpack configuration to ensure compatibility
+  webpack: (config, { dev, isServer }) => {
+    // Disable SWC loader
+    config.module.rules.forEach((rule) => {
+      if (rule.use && rule.use.loader === 'next-swc-loader') {
+        rule.use.loader = 'babel-loader';
+      }
+    });
+    
+    return config;
+  },
 };
 
 module.exports = nextConfig;

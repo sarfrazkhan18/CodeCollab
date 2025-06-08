e87e838ccc70fe580b62a6880caa3c3f4b9b632b
Awan189 <maliksarfrazawan0@gmail.com>
2025-06-08 17:38:51 +0500

    Fix Next.js configuration for static export

--- next.config.js
+++ next.config.js
@@ -8,27 +8,6 @@ const nextConfig = {
   },
   output: 'export',
   reactStrictMode: false,
-  // Completely disable SWC to avoid WebContainer compatibility issues
-  swcMinify: false,
-  compiler: {
-    // Disable SWC transforms
-    removeConsole: false,
-  },
-  // Use Babel instead of SWC
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  // Webpack configuration to ensure compatibility
-  webpack: (config, { dev, isServer }) => {
-    // Disable SWC loader
-    config.module.rules.forEach((rule) => {
-      if (rule.use && rule.use.loader === 'next-swc-loader') {
-        rule.use.loader = 'babel-loader';
-      }
-    });
-    
-    return config;
-  },
 };
 
 module.exports = nextConfig;
